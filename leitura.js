b14d724686c8bcd9440cb33b54bbd5f7e7419c71
Yago <yago.pereira@estudante.ufjf.br>
2023-05-10 17:04:49 -0300

    JS Objetos

--- leitura.js
+++ leitura.js
@@ -2,3 +2,14 @@ const dados = require('./cliente.json')
 
 console.log(dados)
 console.log(typeof dados)
+
+const clienteEmString = JSON.stringify(dados)
+
+console.log(clienteEmString)
+console.log(typeof clienteEmString)
+
+console.log(clienteEmString.nome)
+
+const clienteEmObjeto = JSON.parse(clienteEmString)
+
+console.log(clienteEmObjeto)
